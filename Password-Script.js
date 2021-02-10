5a29d066ce127c7066b31c2df4ae8589c495110a
FaraB <78620899+FaraB-AID@users.noreply.github.com>
2021-02-10 01:01:10 -0500

    Create Password-Script.js

---
+++ Password-Script.js
@@ -0,0 +1,28 @@
+//INPUT MODIFIER
+const modifier = (text) => {    
+  let modifiedText = text
+  const lowered = text.toLowerCase()
+ 
+//Enter your desired password between ""
+  var passWord = ""
+ 
+//Enter what should show up as the prompt if they get the password wrong between ``.
+  var wrongPass = ``
+ 
+//This checks your first scenario input for the password.
+//If it's not there, it deletes your prompt and WI entries.
+if(!state.initial){
+  state.initial = true
+  if (!lowered.includes(passWord)){
+    for(var i = 0; i < worldInfo.length; i++){
+      removeWorldEntry(worldInfo[i].index)
+      i--
+    }
+    modifiedText = wrongPass
+  }
+}
+ 
+ 
+  return ({text : modifiedText})
+}
+modifier(text)
